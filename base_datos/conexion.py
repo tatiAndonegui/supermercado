66af8e585ed553ceed3263473f7b759f98d4ec60
tatiAndonegui <tatiana.andonegui@davinci.edu.ar>
2024-05-21 22:07:03 -0300

    agregamos opcion 1

--- base_datos/conexion.py
+++ base_datos/conexion.py
@@ -6,7 +6,12 @@ class Conexion:
         self.conexion=sqlite3.connect(nombre_bd)
         self.cursor=self.conexion.cursor()
 
+    def crear_tabla_clientes(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS clientes(nombre TEXT, apellido TEXT, dni TEXT)''')
+        self.conexion.commit()
+    
     #cierra la conexion a la bd
     def cerrar_conexion(self):
         self.cursor.close()
         self.conexion.close()
+    
--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from base_datos.conexion import Conexion
 
 print("Bienvenidos al sistema del Supermercado")
 
-opcion=5
-while (opcion!=0):
+opcion=0
+while (opcion!=5):
     try:
         opcion=input("Escoja una opción:\n 1- Agregar cliente \n 2- Editar cliente\n 3-Eliminar cliente\n 4-Mostrar clientes \n 5- Salir\n")
+        if (opcion =="1"):
+            nombre=print("Ingrese el nombre del cliente")
+            apellido=print("Ingrese el apellido del cliente")
+            dni=print("Ingrese el dni del cliente")
+            conexion=Conexion("base_datos/supermercado.db")
+            conexion.crear_tabla_clientes()
         if (opcion =="5"):
             print("saliendo")
     except:
